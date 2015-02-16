9eef9e740e81d727ece1605e83b84cc56c8f02f3
Sasha <sashadee@users.noreply.github.com>
2015-02-16 22:09:34 +1100

    fixed division causing rounding errors angle was being cast to an int, however needed float accuracy on division

--- cse231_draw.py
+++ cse231_draw.py
@@ -9,6 +9,7 @@ __author__ = "sasha"
 import os
 import turtle
 
+
 print """
 This program draws shapes
 Question time:
@@ -16,7 +17,7 @@ Question time:
 #square_count = raw_input()
 shape_sides = int(raw_input("How many sides should the shape have?:"))
 
-if isinstance(shape_sides, (int)):
+if isinstance(shape_sides, (int,float)):
     print "Nice number, lets go!"
 else:
     print "Bad number - please try again"
@@ -24,26 +25,21 @@ else:
 
 
 #geometry
-shape_length = 50
-shape_width = 50
+shape_length = 15
 
 window = turtle.Screen()
 my_turtle = turtle.Turtle()
 
 #draw the shape - actual is the real angle
-shape_internal_angle_actual = ((shape_sides - 2) * 180) / shape_sides
+shape_internal_angle_actual = ((shape_sides - 2) * 180) / float(shape_sides)
 shape_internal_angle_for_turtle = 180 - shape_internal_angle_actual
 
-
 print "drawing a shape with %i sides and an internal angle of %i" % (shape_sides, shape_internal_angle_actual)
-
 
 if (int(shape_sides) % 2 == 0):
     #draw shape with even number of sides
     for i in range(0,shape_sides):
         my_turtle.forward(shape_length)
-        my_turtle.left(shape_internal_angle_for_turtle)
-        my_turtle.forward(shape_width)
         my_turtle.left(shape_internal_angle_for_turtle)
 
     #draw shape with odd number of sides
@@ -52,6 +48,7 @@ elif (int(shape_sides) % 1 == 0):
     for i in range(0,(shape_sides)):
         my_turtle.forward(shape_length)
         my_turtle.left(shape_internal_angle_for_turtle)
+        print "%f %f" % (shape_sides, shape_internal_angle_for_turtle)
 
 turtle.done()
 
